7fbef964d82795a6d663ffb9195334f71cbfb5ea
Ivan Soares <soaresbest@gmail.com>
2014-08-27 11:45:23 -0300

    Novo projeto para repositorio via file system.

--- DDDComTestes.Infraestrutura.Repositorio.FileSystem/Properties/AssemblyInfo.cs
+++ DDDComTestes.Infraestrutura.Repositorio.FileSystem/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DDDComTestes.Web")]
+[assembly: AssemblyTitle("DDDComTestes.Infraestrutura.Repositorio.FileSystem")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DDDComTestes.Web")]
+[assembly: AssemblyProduct("DDDComTestes.Infraestrutura.Repositorio.FileSystem")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e9b9cde5-afda-4e18-89de-17c7d4c6fc45")]
+[assembly: Guid("6ea5a8c7-a55b-420c-9f5d-4dfa178c0904")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
